ab1899e93cf3d95831448659ee1112108ff5893a
Quantum LIu <liuyiliang100@sina.com>
2017-12-28 05:28:38 +0800

    Update get_video.py

--- get_video.py
+++ get_video.py
@@ -5,6 +5,7 @@ Created on Sat Nov 18 09:51:20 2017
 @author: Quantum Liu
 """
 import re,requests
+import argpase
 def get_video(videopage):
     #从播放页抓取视频详情，返回一个图片来了包括不同质量的MP4文件地址的一个字典，时长(秒)，一个包含浏览数、赞/踩比率和数量以及所属分类的字典。可单独使用。
     #Crawling detail of the video, return a tuple which has a dict of MP4 file's address of different qualities, the duration(second),a dict contains number of views, the rate and number of votes Up/Down.
@@ -29,3 +30,17 @@ def get_video(videopage):
     title=re.findall(r_title,html_text)[0]
     infos={'duration':duration,'cover':re.findall(r_cover,html_text)[0].replace('\\',''),'views':re.findall(r_view,html_text)[0],'percent':re.findall(r_percent,html_text)[0],'up':re.findall(r_up,html_text)[0],'down':re.findall(r_down,html_text)[0],'categories':re.findall(r_cate,html_text)}
     return (adds,title,infos)
+
+def show_infos(adds,title,infos):
+    text='Video title:{}\nDuration:{}\nAvailable qualities:{}\nCategories:{}'
+    cats=','.join(infos['categories'])
+    duration=infos['duration']
+    qualities='\n'.join([str(q)+' : '+str(url) for q,url in adds.items()])
+    text=text.format(title,duration,qualities,cats)
+    print(text)
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', type=str, help='Target URL')
+    args = parser.parse_args()
+    show_infos(*get_video(args.url))
